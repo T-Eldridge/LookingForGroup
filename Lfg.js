020272d751c4d97ca64f36b6577b41d14bb6ce5e
TEldridge <skippydrumskins@yahoo.com>
2020-07-06 11:39:06 -0500

    intial direction for capstone

---
+++ Lfg.js
@@ -0,0 +1,5 @@
+// fetch game api Data onload
+// filter api data by onkeyup
+// save LFG submit form to database
+// search LFG database with keywords optioin I.E. (bard, healer, DPS, tank ect.)
+// respond to searched LFGs
